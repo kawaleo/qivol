29edc65899fbb9f4629266d4ccba4fd1e3e50836
kawaleo <bradyhirschey1@gmail.com>
2024-01-09 16:30:02 -0500

    please work loops

--- src/main.rs
+++ src/main.rs
@@ -19,9 +19,87 @@ impl Interpreter {
             match self.src[0] {
                 '=' => self.parse_assignment(),
                 '#' => self.parse_stdout(),
+                '<' => self.parse_loop(),
                 _ => unimplemented!(),
             }
         }
+    }
+
+    fn parse_loop(&mut self) {
+        let mut output = String::new();
+        self.consume('[');
+        while self.src[0] != ']' {
+            match self.src[0] {
+                '#' => output = self.parse_reg_access(),
+                _ => unimplemented!(),
+            }
+        }
+
+        self.expect(']');
+        self.expect('<');
+
+        let mut times = match self.src[0] {
+            '[' => {
+                let reg_idx = self.get_reg();
+
+                self.expect('>');
+                reg_idx
+            }
+            _ => {
+                let mut reg_idx = String::new();
+
+                while self.src[0] != '>' {
+                    if !self.src[0].is_digit(10) {
+                        process::exit(1);
+                    }
+                    reg_idx.push(self.src.remove(0));
+                }
+
+                self.expect('>');
+                reg_idx.parse::<usize>().unwrap()
+            }
+        };
+
+        while times > 0 {
+            print!("{}", output);
+            times -= 1
+        }
+    }
+
+    fn get_reg(&mut self) -> usize {
+        self.expect('[');
+
+        let mut reg_val = String::new();
+
+        while self.src[0] != ']' {
+            if !self.src[0].is_digit(10) {
+                std::process::exit(1)
+            }
+
+            reg_val.push(self.src.remove(0));
+        }
+
+        self.expect(']');
+
+        reg_val.parse::<usize>().unwrap()
+    }
+
+    fn parse_reg_access(&mut self) -> String {
+        self.consume('[');
+        let mut idx = String::new();
+
+        while self.src[0] != ']' {
+            if !self.src[0].is_digit(10) {
+                std::process::exit(1);
+            }
+
+            idx.push(self.src.remove(0));
+        }
+
+        self.expect('>');
+        let idx = idx.parse::<usize>().unwrap();
+
+        format!("{}", self.register[idx - 1])
     }
 
     fn parse_stdout(&mut self) {
